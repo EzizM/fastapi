b8b6b59d4024b221a3c9c0e9f6028971830025d8
EzizM <turkmenpsycho@gmail.com>
2026-09-03 00:02:20 +0500

    fourth commit

--- db/connect.py
+++ db/connect.py
@@ -1,11 +1,8 @@
 from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
-from dotenv import load_dotenv
 import os
 
-load_dotenv()
-
-DATABASE_URL = os.getenv("sqlite+aiosqlite:///./base.db")
+DATABASE_URL = "sqlite+aiosqlite:///./base.db"
 
 engine = create_async_engine(DATABASE_URL, echo=False)
 
